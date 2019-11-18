e2457e7086975d0182a2723f88718901ef70807c
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-11-18 18:09:43 -0500

    migrations dones and tables up and running

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -105,9 +105,9 @@ DATABASES = {
 }
 
 
-DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
-}
+# DATABASES = {
+#     'default': dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
+# }
 
 
 # Password validation
