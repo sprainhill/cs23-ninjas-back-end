02bcda10335f938db446f176a97d2414f3322309
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-11-19 11:48:51 -0500

    More attempts to fix bug in staticfiles at settings

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -166,7 +166,7 @@ STATIC_URL = '/staticfiles/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles/img')
 ]
 
 import django_heroku
