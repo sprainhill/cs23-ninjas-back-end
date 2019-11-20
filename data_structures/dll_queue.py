2b9ff018bb953b9fb338bd7bbd4047224011c5dd
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-11-20 13:14:34 -0800

    Import data structures Co-authored-by: Rory <rory.christian.murray@gmail.com>

---
+++ data_structures/dll_queue.py
@@ -0,0 +1,24 @@
+from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('./doubly_linked_list')
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = DoublyLinkedList()
+
+    def enqueue(self, value):
+        self.size += 1
+        self.storage.add_to_tail(value)
+
+    def dequeue(self):
+        value = self.storage.remove_from_head()
+
+        if value is not None:
+            self.size -= 1
+
+        return value
+
+    def len(self):
+        return self.size
