3862b8018463cb622f2f7c5566fca26134d76bf0
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-11-21 21:09:41 -0500

    Add Pusher import

--- adventure/models.py
+++ adventure/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 import uuid
+from pusher import Pusher
 
 
 class Room(models.Model):
